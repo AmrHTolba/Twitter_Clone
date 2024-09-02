cc3cf05d49c5703a0b1ac5bd4c7d703ce09dea21
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-09-02 20:50:55 +0300

    Created Other tab views
    More tab views created and can be changed based on tap selection

---
+++ TwitterClone/Core/Messages/MessagesView.swift
@@ -0,0 +1,18 @@
+//
+//  MessagesView.swift
+//  TwitterClone
+//
+//  Created by Amr El-Fiqi on 02/09/2024.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text("Messages View")
+    }
+}
+
+#Preview {
+    MessagesView()
+}
