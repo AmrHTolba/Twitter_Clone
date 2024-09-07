c73e6ae0750e11e52d8a21749e2bc56174afbf58
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-09-07 09:20:53 +0300

    Explore View & User Row View
    Created a user row view to show inside the explore view

---
+++ TwitterClone/Core/Explore/Views/ExploreView.swift
@@ -0,0 +1,35 @@
+//
+//  ExploreView.swift
+//  TwitterClone
+//
+//  Created by Amr El-Fiqi on 02/09/2024.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScrollView {
+                    LazyVStack {
+                        ForEach( 0 ... 25, id: \.self) { _ in
+                            NavigationLink {
+                                ProfileView()
+                            } label: {
+                                UserRowView()
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle("Explore")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
