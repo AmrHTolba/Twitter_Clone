d2d95322386b694619ebecd5239bf4797bf0d38d
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-09-08 12:09:46 +0300

    Removed Unnecessary nav stack

--- TwitterClone/TwitterCloneApp.swift
+++ TwitterClone/TwitterCloneApp.swift
@@ -11,9 +11,7 @@ import SwiftUI
 struct TwitterCloneApp: App {
     var body: some Scene {
         WindowGroup {
-            NavigationStack{
                 ContentView()
-            }
         }
     }
 }
